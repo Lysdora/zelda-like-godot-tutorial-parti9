82713f078f9d28211b3607b99731d8474b685e85
Lysdora-craft <lysdora.web@gmail.com>
2025-11-20 11:22:43 +0100

    commit ui instance

--- ui/scripts/dialogue_box.gd
+++ ui/scripts/dialogue_box.gd
@@ -1,11 +1,25 @@
 extends MarginContainer
 
+@onready var nom_pnj_label = $PanelContainer/VBoxContainer/NomPNJ
+@onready var texte_label = $PanelContainer/VBoxContainer/TexteDialogue
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("Test pour voir si ca marche ")
+	# Cacher la boite de dialogue au démarrage
+	hide()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed('ui_accept'):
+		afficher_dialogue("Bilbo", "Tu as appuyé sur ESPACE !")
+
+	
+
+func afficher_dialogue(nom: String, texte: String):
+	nom_pnj_label.text = nom
+	texte_label.text = texte
+	show()
+
+func fermer_dialogue():
+	hide()
